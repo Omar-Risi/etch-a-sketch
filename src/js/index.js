e2ec6919a6b0c4c7c9a71af92c92816e3f046c04
Omar Risi <orisi4business@gmail.com>
2024-02-08 21:30:13 +0400

    added click event for the boxes

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,40 @@
 //global variables 
-const grid_array = [];
+const grid_container = document.querySelector(".container");
+let grid_array = [];
+
+// add click event to box
+function boxClick (box) {
+
+    box.style.backgroundColor="#000";
+}
 
 //create grid function
 function createGrid(grid_size) {
 
+    //configure container
+    grid_container.innerHTML = "";
+    grid_container.style.cssText = `
+                                    grid-template-rows: repeat(${grid_size},1fr);
+                                    grid-template-columns:repeat(${grid_size},1fr)`;
+
+
     let grid_boxes = [];
-    for(let i = 0; i < grid_size; i++) {
+    for(let i = 0; i < (grid_size * grid_size); i++) {
 
         const gridBox = document.createElement("div");
         gridBox.classList.add("grid-box");
         gridBox.id = `box-${i}`;
+        gridBox.onclick = () => {boxClick(gridBox)};
         grid_boxes.push(gridBox);
-        
-        //add to GridContainer
+        grid_array = grid_boxes;
+
+        grid_container.appendChild(gridBox);
     }
+
 
 
     return grid_boxes;
 }
 
 
-console.log(createGrid(32))
+createGrid(32);
